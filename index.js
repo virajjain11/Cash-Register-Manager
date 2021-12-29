2d247ee6b3a855124885fb703575a17dfde2c5b2
Viraj Jain <virajjain7.vj@gmail.com>
2021-12-29 19:27:41 +0530

    change logic in js

--- index.js
+++ index.js
@@ -1,42 +1,91 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const button = document.querySelector("#check");
-const message = document.querySelector(".error-message")
+const buttonNext = document.querySelector(".btn-next");
+const divCash = document.querySelector("#cash");
+const  table =document.querySelector("#table");
+
+const message = document.querySelector(".error-message-check");
+const nextMsg = document.querySelector(".error-message-next");
 const notes = [2000,500,200,100,50,20,10,5,1];
 const noNotes = document.querySelectorAll(".no-of-notes");
 
-button.addEventListener("click", validateBillAndCash )
+hideCashAndTable();
+buttonNext.addEventListener("click", validateBillAmount);
+// button.addEventListener("click", validateBillAndCash );
+button.addEventListener("click",  validateCash );
 
-function validateBillAndCash(){ 
-    hideMsg();
+function validateBillAmount(){
+    hideCashAndTable();
     if(billAmount.value>0){
-       if(cashGiven.value>= billAmount.value){
-          const amountReturned = cashGiven.value - billAmount.value;
-          calculateChange(amountReturned);
-       }else{
-           displayMsg("cash given should be greater than bill amount");
-       }
+    divCash.style.display ="block";
+    message.style.display = "none";
+    buttonNext.style.display = "none";
+    } else{
+        nextMsg.style.display = "block";
+        nextMsg.innerHTML = "Invalid Bill Amount";
     }
-    else{
-         displayMsg("Invalid Bill Amount");
+}
+function validateCash(){
+    if((cashGiven.value - billAmount.value)<0){
+        message.style.display = "block";
+      message.innerHTML = "Cash given should be more than bill amount"; 
+      table.style.display="none";
+    } else{
+        table.style.display="block";
+        const amountReturned = cashGiven.value - billAmount.value;
+        calculateChange(amountReturned);
+    }
+}
 
-    }
+function hideCashAndTable(){
+    nextMsg.style.display = "none";
+    divCash.style.display ="none";
+    table.style.display="none";
 }
 function calculateChange(amountRet){
-    for(i=0;i<notes.length;i++){
-        const noOfNotes = Math.trunc(amountRet/notes[i]);
-        amountRet%=notes[i];
-        noNotes[i].innerText=noOfNotes;
-
-
+        for(i=0;i<notes.length;i++){
+            const noOfNotes = Math.trunc(amountRet/notes[i]);
+            amountRet %= notes[i];
+            noNotes[i].innerText = noOfNotes;
+        }
     }
 
-}
-function hideMsg(){
-    message.style.display = "none";
-}
 
-function displayMsg(msg){
-    message.style.display = "block";
-    message.innerHTML = msg;
-}
+
+
+
+
+
+// function validateBillAndCash(){ 
+//     hideMsg();
+//     if(billAmount.value>0){
+//        if((cashGiven.value>billAmount.value)||(cashGiven.value==billAmount.value)){
+//           const amountReturned = cashGiven.value - billAmount.value;
+//           calculateChange(amountReturned);
+//        } else{
+//            displayMsg("cash given should be greater than bill amount");
+//        }
+//     } else{
+//          displayMsg("Invalid Bill Amount");
+
+//     }
+// }
+// function calculateChange(amountRet){
+//     for(i=0;i<notes.length;i++){
+//         const noOfNotes = Math.trunc(amountRet/notes[i]);
+//         amountRet%=notes[i];
+//         noNotes[i].innerText=noOfNotes;
+
+
+//     }
+
+// }
+// function hideMsg(){
+//     message.style.display = "none";
+// }
+
+// function displayMsg(msg){
+//     message.style.display = "block";
+//     message.innerHTML = msg;
+// }
